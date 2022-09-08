6ab435acb5be18eddedaa6af5474d04f73c3f916
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-08 16:18:23 -0700

    My first C commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Starting point
+ *
+ * Description: A C program that prints a quote
+ *
+ * Return: Always 1(Success)
+ */
+
+int main(void)
+{
+	write(2,"and that pieceof art is useful\"-Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
