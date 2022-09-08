c9c6017462349f9f4c70c50fc259f6b198fc88c5
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-08 14:44:04 -0700

    My first C commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Starting point
+ *
+ * Description: Writing a C program that prints with the function printf
+ *
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	retun (0);
+}
