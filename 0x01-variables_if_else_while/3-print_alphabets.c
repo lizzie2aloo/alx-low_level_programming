66831bba9ce1d329a8a93712d1a48339c530f610
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-10 13:37:49 -0700

    Variables commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Description: print alphabets in lowercase then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	int x, y;
+	char ch = 'a';
+       char CH = 'A';
+/*prints a - z*/
+while (ch <= 'z')
+{
+putchar(ch);
+++ch;
+}
 
-	<<<<<<< HEAD
-		for (m = 97; m <= 122; m++)
-			putchar(m);
-	for (n = 65; n <= 90; n++)
-		putchar(n);
-	=======
-		for (x = 97; x<= 122; x++)
-		{
-			putchar(x);
-		}
-	for (y = 65; y <= 90; y++)
-	{
-		putchar(y);
-	}
-	>>>>>>> c38d008b18676f16774155151031a0fcc1c46752
-		putchar('\n');
-	return (0);
+/*print A - Z*/
+while (CH <= 'Z')
+{
+	putchar(CH);
+	++CH;
+}
+putchar('\n');
+
+return (0);
 }
