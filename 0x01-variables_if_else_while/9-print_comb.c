6e52b0f45a4cc826a91125a659aab039b83ea4b2
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-10 14:52:54 -0700

    Variable commits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Description: print 0, 1, -9
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	int k;
-
-	for (k = 48; k <= 57; k++)
+	int digit = 0;
+	while (digit <= 9)
 	{
-		putchar(k);
-		if (k < 57)
+		putchar(digit + 48);
+
+		if (digit != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+		++digit;
 	}
 	putchar('\n');
+
 	return (0);
 }
