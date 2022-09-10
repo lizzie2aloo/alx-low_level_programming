b085ccc938755dc8670273fe114ceaa9c1f1a37c
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-10 13:55:45 -0700

    Variables commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Description: print all alphabet letters except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	int y;
+	char ch = 'a';
 
-	for (y = 97; y <= 122; y++)
+	while (ch <= 'z')
 	{
-		<<<<<<< HEAD
-			       if (m == 101 || m == 113)
-				       =======
-					       if (y == 101 || y == 113)
-						       >>>>>>> c38d008b18676f16774155151031a0fcc1c46752
-						       {
-						       }
-					       else
-					       {
-						       putchar(y);
-					       }
+		if (ch == 'e' || ch == 'q')
+			++ch;
+		putchar(ch);
+		==ch;
 	}
 	putchar('\n');
+
 	return (0);
 }
