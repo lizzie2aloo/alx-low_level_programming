da8330adb2710a549daf051083d3a1503a6bd05a
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-13 12:19:22 -0700

    Functions nested loops commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10-functions that prints 10times the alphabet
+ * print_alphabet_x10 - -function
  *
- * Description: in lowercase
+ * Decription: function that prints alphabet letters ten times
  *
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
 {
-	int line, ch;
+	int counter = 0;
+	int letter;
 
-	for (line = 0; line <= 9; ++line)
+	while (counter++ <= 9)
 	{
-		for (ch = 'a'; ch <= 'z'; ++ch)
-			_putchar(ch);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
 		_putchar('\n');
 	}
 }
