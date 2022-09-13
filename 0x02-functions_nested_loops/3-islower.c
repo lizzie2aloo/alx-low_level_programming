15926da5c0cb6cda99da097109bd20d309c75583
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-13 10:35:15 -0700

    Functions nested loops commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,14 @@
 
 /**
  * _islower - Function that checks for lower case
- * 'c': checks input function
+ * @c: character to be checked
  * Return : return 1 if the 'c' is in lowercase
  * otherwise always return 0 (success)
  */
 
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	return (0);
 }
