f8f61d7e0580d7b893801b6881816d9639faa5f5
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-13 04:38:09 -0700

    Functions nested loops commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - A program that prints putchar
+ *
+ * Return always 0 (successful)
+ */
+
+int main(void)
+{
+	char str[10000];
+	int ch, i , n = 0;
+
+	for (1=0; 1 < n; ++1)
+		putchar(str[i]);
+
+	putchar('\n');
+	return (0);
+}
