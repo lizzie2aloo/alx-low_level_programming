60b0a22c5cf656a0ba4169095bacf5cf9c238dab
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-14 13:07:10 -0700

    Functions nested loops commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,16 +1,13 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main- Entry point
- *
- * Description: computes the sum of all multiples of 3 or 5
- * below 1024 (excluded)
- * Return: Always 0
+ * main - count of multiples of 3 0r 5
+ * Return: return 0
  */
-
 int main(void)
 {
 	int n, sum = 0;
+
 	for (n = 0; n < 1024; n++)
 	{
 		if ((n % 3) == 0 || (n % 5) == 0)
