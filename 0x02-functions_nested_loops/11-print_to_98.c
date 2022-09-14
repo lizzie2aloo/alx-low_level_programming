d9b1487594a08caf014ddb5c6b40c2bba624b660
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-14 11:29:17 -0700

    Functions nested loops commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - print n to 98 counts
+ * print_to_98 - function that prints n to 98 counts
  * followed by a new line
  * @n: print from this number
+ * Return: 0
  */
 
 void print_to_98(int n)
 {
-	int count;
 
 	if (n >= 98)
 	{
@@ -21,6 +21,6 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 			printf("%d", n++);
-		printf("%d\n", n):
+		printf("%d\n", n);
 	}
 }
