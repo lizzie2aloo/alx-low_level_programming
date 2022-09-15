fa6a9267c6c77e7db91983f7276375cdb7feeb99
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-15 12:06:58 -0700

    More functions nested loops commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
+
 #include "main.h"
 
+
+
 /**
- * print_numbers - print 0 - 9
- * only using _putchar twice
- * Return: Always 0 (Success)
- */
+
+*print_numbers - function that print all numbers from
+
+*0 to 9
+
+*
+
+*Return: returns nothing
+
+*/
+
+
 
 void print_numbers(void)
+
 {
+
 	int n;
 
-	for (n =48; n < 58; n++)
+
+
+	for (n = 48; n < 58; n++)
+
 	{
-		_putchar(n);
+
+		putchar(n);
+
 	}
-	_putchar(10);
+
+	putchar(10);
+
 }
+^C
+C
