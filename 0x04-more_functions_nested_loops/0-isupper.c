91d3ed46e6b6f2541ac86bef2e18962aff8a9689
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-15 11:47:43 -0700

    More functions nested loops commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * _isupper - check if c is upper
- * @c: input for alphabet
- *
- * Return: 1 if its uppercase, 0 if not
+ * _isupper - checks if a  character is uppercase or not
+ * @c: character to be tested
+ * Return: 1 whether it is, 0 otherwise
  */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if ((c >= 'A') && (c <= 'Z'))
+	{
 		return (1);
-	else
-		return (0);
+	}
+
+	return (0);
 }
