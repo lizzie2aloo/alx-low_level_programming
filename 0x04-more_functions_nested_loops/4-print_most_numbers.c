c245b81bd77f06fc112647d62de952e0bc7bc472
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-15 13:22:19 -0700

    More functions nested loops commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print 0 - 9 apart from 2 and 4 
+ * print_most_numbers - print numbers from 0 to 9 except 2 and 4
+ *
  * Return: void
  */
 void print_most_numbers(void)
