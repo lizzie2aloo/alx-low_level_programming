c454a67b57b158b361b90329c8404ed02839a381
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-19 02:55:51 -0700

    pointers arrays strings commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,5 @@
 /**
- * _strlen - returns the length of s string
+ * _strlen - returns the length of a string
  * @s: string parameter input
  * Return: Length of a spring
  */
@@ -11,5 +11,5 @@ int _strlen(char *s)
 	for (counter = 0; *s != '/0'; ++s)
 		++counter;
 
-	return +(counter);
+	return (counter);
 }
