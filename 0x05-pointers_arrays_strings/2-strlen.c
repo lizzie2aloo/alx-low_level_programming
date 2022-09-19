3a8bd837431e4d04121c5061a555a65842338372
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-19 02:44:26 -0700

    pointers arrays springs commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen - returns tge length of s string
+ * @s: string parameter input
+ * Return: Length of a spring
+ */
+	
+int _strlen(char *s)
+{
+	char str[];
+	int = s
+
+	for (s = 0; str[s] != '/0'; ++s);
+
+	return (s)
+}
