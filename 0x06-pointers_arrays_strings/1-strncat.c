aff3d65f90112bab31e4a76d40d9a38498f82aa9
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-21 23:02:48 -0700

    1-strncat commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,32 +1,22 @@
 #include "main.h"
 
 /**
- * _strncat - function that concatenates two strings
- * @dest: A pointer to a character that will be changed
- * @src: A pointer to a character that will also be changed
- * @n: value
+ * _strncat - concatenate two strings
+ * @src: source
+ * @dest: destination
+ * @n: integer
  * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int index = 0, dest_len = 0;
 
-	i= 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
+	while (dest[index++])
+		dest_len++;
 
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-	
-	dest[i] = '\0';
-
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
+
 }
