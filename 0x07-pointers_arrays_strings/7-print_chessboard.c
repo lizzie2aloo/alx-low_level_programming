8f69a88882f2cf8bd8979eca90d57804cc6a599e
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-26 13:10:38 -0700

    print chessboard commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,17 +10,10 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	i = 0;
-	j = 0;
-	while (i < 64)
+	for (i = 0; a[i][7]; i++)
 	{
-		if (i % 8 == 0 && i != 0)
-		{
-			j = i;
-			_putchar('\n');
-		}
-		_putchar(a[i / 8][i - j]);
-		i++;
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
