773dc0a0a037af00329d4841e3264a50e61da023
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-26 13:07:52 -0700

    print chessboard commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints 2D array of chessboard
+ * @a: chessboard array
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (i < 64)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			j = i;
+			_putchar('\n');
+		}
+		_putchar(a[i / 8][i - j]);
+		i++;
+	}
+	_putchar('\n');
+}
