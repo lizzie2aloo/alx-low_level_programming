74bc2dba46cdf660b0753916bfbd1e1a9191d9df
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-26 12:24:20 -0700

    task memset commit

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * char prototypes(char)
+ * int prototypes(int)
+ * void prototypes(void)
+ */
+char *_memset(char *s, char b, unsigned int n);
+#endif /* MAIN_H */
