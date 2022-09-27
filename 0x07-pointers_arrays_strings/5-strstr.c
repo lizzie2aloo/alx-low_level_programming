5a07a0f1b2c0138c9b57f7b8b8b142ddedf21cb3
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-27 11:43:00 -0700

    5strstr commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -20,13 +20,11 @@ char *_strstr(char *haystack, char *needle)
 
 		if (haystack[i] == needle[i])
 		{
-			do
-			{
+			do{
 				if (needle[i + 1] == '\0')
 					return (haystack);
 				i++;
-			}
-			while (haystack[i] == needle[i]);
+			} while (haystack[i] == needle[i]);
 		}
 		haystack++;
 	}
