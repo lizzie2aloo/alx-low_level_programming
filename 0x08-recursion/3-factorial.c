3a7375d87f32d6b6ee406b31db5341a63342c0f7
lizzie2aloo <shantoulartistics@gmail.com>
2022-09-28 12:44:03 -0700

    recursion commit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,13 +8,15 @@
  */
 int factorial(int n)
 {
+	int result;
+
 	if (n < 0)
-		/*error -1 if number is negative*/
 		return (-1);
-	else if (n == 0)
-		/*return factorial of 0 = 1*/
+
+	else if (n >= 0 && n <= 1)
 		return (1);
-	else
-		/*do factorial of n if its not less or equal to 0*/
-		return (n * factorial(n - 1));
+
+	result *= factorial(n - 1);
+
+	return (result);
 }
