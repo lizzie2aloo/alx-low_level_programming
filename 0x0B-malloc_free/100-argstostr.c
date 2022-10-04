9adb8a5a354f7f48efa88c8e6e300368f07874ee
lizzie2aloo <shantoulartistics@gmail.com>
2022-10-04 07:54:43 -0700

    malloc free task commit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,24 +12,24 @@ char *argstostr(int ac, char **av)
 {
 	char *new_string = NULL;
 	int k = 0, i = ac, j, sum = 0, temp = 0;
-	
+
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	
+
 	while (ac--)
 		sum += (len(av[ac]) + 1);
 	new_string = (char *) malloc(sum + 1);
-	
+
 	if (new_string != NULL)
 	{
 		while (k < i)
-	{
-		for (j = 0; av[k][j] != '\0'; j++)
-			new_string[j + temp] = av[k][j];
-		new_string[temp + j] = '\n';
-		temp += (j + 1);
-		k++;
-	}
+		{
+			for (j = 0; av[k][j] != '\0'; j++)
+				new_string[j + temp] = av[k][j];
+			new_string[temp + j] = '\n';
+			temp += (j + 1);
+			k++;
+		}
 		new_string[temp] = '\0';
 	}
 	else
@@ -49,7 +49,7 @@ char *argstostr(int ac, char **av)
 int len(char *str)
 {
 	int len = 0;
-	
+
 	if (str != NULL)
 	{
 		while (str[len])
