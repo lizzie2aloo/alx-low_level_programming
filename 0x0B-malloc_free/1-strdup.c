bb88a5ed6064eb93ddaf3886ddbdec3743f9e08a
lizzie2aloo <shantoulartistics@gmail.com>
2022-10-04 06:56:35 -0700

    malloc free task commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,21 +14,21 @@ char *_strdup(char *str)
 {
 	char *duplicate;
 	int i, len = 0;
-	
+
 	if (str == NULL)
 		return (NULL);
-	
+
 	for (i = 0; str[i]; i++)
 		len++;
-	
+
 	duplicate = malloc(sizeof(char) * (len + 1));
-	
+
 	if (duplicate == NULL)
 		return (NULL);
-	
+
 	for (i = 0; str[i]; i++)
 		duplicate[i] = str[i];
 	duplicate[len] = '\0';
-	
+
 	return (duplicate);
 }
