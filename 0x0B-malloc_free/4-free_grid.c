3f1ce4b09ec75bc35084a5467c3d8170f1d30d75
lizzie2aloo <shantoulartistics@gmail.com>
2022-10-04 05:41:35 -0700

    malloc free task commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * free_grid - free the grid
+ * @grid: grid of memories
+ * @height: int
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+	
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	
+	free(grid);
+}
